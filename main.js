c43f90b67385c84f2891248a007bb708dbdfcfe5
Cieliocas <francielioevangelista@hotmail.com>
2024-12-05 03:06:26 -0300

    edit

--- main.js
+++ main.js
@@ -5,4 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const githubRepository = document.querySelector('#repository');
     const githubFollowing = document.querySelector('#following');
     const githubFollowers = document.querySelector('#followers');
+
+    fetch('https://api.github.com/users/cieliocas')
+        .then(response => response.json())
+        .then(data => {
+            githubName.innerHTML = data.name;
+            githubUsername.innerHTML = data.login;
+            githubAvatar.src = data.avatar_url;
+            githubRepository.innerHTML = data.public_repos;
+            githubFollowing.innerHTML = data.following;
+            githubFollowers.innerHTML = data.followers;
+        });
+
+
 })
