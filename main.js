0767627a242c481c9eed75add15ccaa190e635fd
Cieliocas <francielioevangelista@hotmail.com>
2024-12-05 03:04:09 -0300

    start project

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const githubName = document.querySelector('#name');
+    const githubUsername = document.querySelector('#username');
+    const githubAvatar = document.querySelector('#avatar');
+    const githubRepository = document.querySelector('#repository');
+    const githubFollowing = document.querySelector('#following');
+    const githubFollowers = document.querySelector('#followers');
+})
